0368cad8eb761626965c5fe0c5b1d6512e11f56d
wesleymktd <wesleymktd@gmail.com>
2023-07-05 17:53:58 -0300

    método de sacar dinheiro implementado req6 ok

--- src/trybank/Trybank.cs
+++ src/trybank/Trybank.cs
@@ -110,7 +110,22 @@ public class Trybank
     // 6. Construa a funcionalidade de sacar dinheiro
     public void Withdraw(int value)
     {
-        throw new NotImplementedException();
+        if (!Logged)
+        {
+            throw new AccessViolationException("Usuário não está logado");
+        } 
+
+        int withDrawResult = CheckBalance() - value;
+
+        if (withDrawResult < 0) 
+        {
+            throw new InvalidOperationException("Saldo insuficiente");
+        } 
+        else
+        {
+            Bank[loggedUser, 3] -= value;
+        }
+        
     }
 
     // 7. Construa a funcionalidade de transferir dinheiro entre contas
